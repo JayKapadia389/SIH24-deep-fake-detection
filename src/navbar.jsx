c6eaf64cd9479228e5f6796b3264aa960f90059e
Jay <jaykapadia389@gmail.com>
2024-08-25 14:15:10 +0530

    navbar component added

---
+++ src/navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './navbar.css'
+
+function Navbar() {
+
+  return (
+    <div className="navbar">
+      navbar
+    </div>
+  )
+}
+
+export default Navbar
