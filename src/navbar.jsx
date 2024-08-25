1bf4e1e37f575c757ddf0c96410af07145e79b6d
SutharMahendra <omajimahendra@gmail.com>
2024-08-25 14:57:48 +0530

    navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
     return (
+
         <section className="navbar-look">
             <div className="container">
                 <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
